6bd855a74d5fd3f4ef047dcfd6de77d9c218f591
Richard Wong <ricwtk@gmail.com>
2026-07-19 09:36:14 +0800

    update current path display

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Button } from './components/ui/button';
 
-import { useRouter } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
+const currentRoute = useRoute()
 const router = useRouter();
 const routerList = router.getRoutes();
 
@@ -10,8 +11,11 @@ const user = "user";
 
 <template>
   <div class="flex flex-row px-5 py-3 border-b border-border gap-2 items-center">
-    <div class="font-black">SED Poster</div>
-    <Button variant="ghost" v-for="route in routerList" :key="route.name" :to="route.path">{{ route.name }}</Button>
+    <div class="font-black">SED</div>
+    <Button as-child variant="ghost" v-for="route in routerList" :key="route.name"
+      :class="currentRoute.name == route.name ? 'bg-teal-600 hover:bg-teal-100 text-white hover:text-primary' : ''"
+      class="sm:visible md:hidden"
+    ><RouterLink :to="route.path">{{ route.name }}</RouterLink></Button>
     <div class="flex-1"></div>
     <div>Logged in as <span class="font-bold">{{ user }}</span></div>
     <Button variant="destructive">Log out</Button>
