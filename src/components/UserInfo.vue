05d4fc424ed1e22264c2a4a4597938fd2d4c2ad4
Richard Wong <ricwtk@gmail.com>
2026-07-20 22:19:36 +0800

    update multiple select in user list

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,63 +1,83 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { PhPen, PhCheck, PhX, PhTrash } from '@phosphor-icons/vue'
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent, CardFooter, CardDescription } from '@/components/ui/card';
 import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input'
 import type { User } from '@/types'
-import TagsInputWithListbox from './TagsInputWithListbox.vue'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+// import TagsInputWithListbox from './TagsInputWithListbox.vue'
 
 const props = defineProps<{
-  user: User
+  user: User,
+  rubricList: string[],
+  teamList: string[],
 }>()
+const role = ref(props.user.role)
+const rubricIds = ref(props.user.rubricIds ?? [])
+const teamIds = ref(props.user.teamIds ?? [])
+
 const editing = ref(false)
 const confirmEdit = () => { editing.value = false }
-const cancelEdit = () => { editing.value = false }
+const cancelEdit = () => {
+  role.value = props.user.role
+  rubricIds.value = props.user.rubricIds ?? []
+  teamIds.value = props.user.teamIds ?? []
+  editing.value = false
+}
 const deleteUser = () => { editing.value = false }
+
 </script>
 
 <template>
 <Card>
   <CardHeader>
     <CardTitle>{{ user.uid }}</CardTitle>
-    <CardSubtitle>{{ user.displayName }}</CardSubtitle>
+    <CardDescription>{{ user.displayName }}</CardDescription>
   </CardHeader>
   <CardContent>
     <FieldGroup>
       <Field>
         <FieldLabel>Role</FieldLabel>
-        <Input type="text" v-model="user.role"
+        <Input type="text" v-model="role"
           :readonly="!editing"
           class="togglable-input"
         />
       </Field>
       <Field>
         <FieldLabel>Rubrics</FieldLabel>
-        <Input type="text"
-          :readonly="!editing"
-          class="togglable-input"
-        />
-        <TagsInputWithListbox v-model="user.rubricIds"/>
+        <Select multiple v-model="rubricIds">
+          <SelectTrigger class="w-full togglable-select disabled:[&_svg]:opacity-0" :disabled="!editing">
+            <SelectValue placeholder="Select rubrics" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="rubric in rubricList" :key="rubric" :value="rubric">{{ rubric }}</SelectItem>
+          </SelectContent>
+        </Select>
       </Field>
       <Field>
         <FieldLabel>Teams</FieldLabel>
-        <Input type="text"
-          :readonly="!editing"
-          class="togglable-input"
-        />
+        <Select multiple v-model="teamIds">
+          <SelectTrigger class="w-full togglable-select disabled:[&_svg]:opacity-0" :disabled="!editing">
+            <SelectValue placeholder="Select teams" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem v-for="team in teamList" :key="team" :value="team">{{ team }}</SelectItem>
+          </SelectContent>
+        </Select>
       </Field>
     </FieldGroup>
   </CardContent>
   <CardFooter>
-    <div class="ms-auto flex flex-row gap-2">
+    <div class="flex flex-row gap-2 w-full">
       <template v-if="!editing">
-        <Button variant="default" @click="editing = true"><PhPen/>Edit</Button>
+        <Button variant="default" @click="editing = true" class="ms-auto"><PhPen/>Edit</Button>
       </template>
       <template v-else>
+        <Button variant="destructive" @click="deleteUser" class="me-auto"><PhTrash/>Delete</Button>
         <Button variant="default" @click="confirmEdit"><PhCheck/>Confirm</Button>
         <Button variant="secondary" @click="cancelEdit"><PhX/>Cancel</Button>
-        <Button variant="destructive" @click="deleteUser"><PhTrash/>Delete</Button>
       </template>
     </div>
   </CardFooter>
@@ -67,5 +87,8 @@ const deleteUser = () => { editing.value = false }
 <style scoped>
 .togglable-input {
   @apply read-only:pointer-events-none read-only:border-transparent read-only:bg-transparent read-only:shadow-none
+}
+.togglable-select {
+  @apply disabled:opacity-100 disabled:pointer-events-none disabled:border-transparent disabled:bg-transparent disabled:shadow-none
 }
 </style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -20,6 +20,9 @@ const otherUsers = [
     teamIds: [''],
   },
 ]
+
+const rubricList = ['a', 'b', 'c']
+const teamList = ['T1-1','T1-2']
 </script>
 
 <template>
@@ -27,14 +30,14 @@ const otherUsers = [
 <Marker variant="separator" class="py-3 select-none">
   <MarkerContent>Current User</MarkerContent>
 </Marker>
-<UserInfo :user="currentUser" class="w-full max-w-md"/>
+<UserInfo :user="currentUser" class="w-full max-w-md" :rubricList="rubricList" :teamList="teamList"/>
 
 <!-- Other users -->
 <Marker variant="separator" class="py-3 select-none">
   <MarkerContent>Other Users</MarkerContent>
 </Marker>
 <div class="flex flex-row flex-wrap">
-  <UserInfo v-for="user in otherUsers" :key="user.uid" :user="user" class="w-full max-w-md" />
+  <UserInfo v-for="user in otherUsers" :key="user.uid" :user="user" class="w-full max-w-md" :rubricList="rubricList" :teamList="teamList"/>
 </div>
 
 </template>
